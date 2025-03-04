653ebaae198cc07e61ab090ec2cebe6a83bd4267
Shahamat Irtisham <167608296+shahamatirtisham@users.noreply.github.com>
2025-03-04 19:13:51 +0600

    Create 731A - Night at the Museum.cpp

---
+++ 731A - Night at the Museum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin>>s;
+    char prev='a';
+    int r=0;
+    for(int i=0;i<s.size();i++)
+    {
+        char curr=s[i];
+        int right=abs(curr-prev);
+        int left=abs(abs(curr-prev)-26);
+        r+=min(right,left);
+        prev=curr;
+    }
+    cout << r;
+}
