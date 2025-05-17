8ea497500e0fbf205b4b8494c20b506272888f01
Shahamat Irtisham <167608296+shahamatirtisham@users.noreply.github.com>
2025-05-17 21:04:58 +0600

    Create 959A - Mahmoud and Ehab and the even.cpp

---
+++ 959A - Mahmoud and Ehab and the even.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    
+    long long n;
+    cin >> n;
+    
+    if (n % 2 == 0) {
+        cout << "Mahmoud\n";
+    } else {
+        cout << "Ehab\n";
+    }
+    
+    return 0;
+}
