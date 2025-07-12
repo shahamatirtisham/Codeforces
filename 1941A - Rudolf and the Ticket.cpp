955c40a0bf14eca571305c9fec7a899ff23f26c4
Shahamat Irtisham <167608296+shahamatirtisham@users.noreply.github.com>
2025-07-12 14:38:20 +0600

    Create 1941A - Rudolf and the Ticket.cpp

---
+++ 1941A - Rudolf and the Ticket.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// map<ll, bool> primes;
+// void sieve(ll n)
+// {
+//     vector<bool> is_prime(n + 1, true);
+//     is_prime[0] = is_prime[1] = false;
+
+//     for (ll i = 2; i * i <= n; ++i)
+//     {
+//         if (is_prime[i])
+//         {
+//             for (ll j = i * i; j <= n; j += i)
+//             {
+//                 is_prime[j] = false;
+//             }
+//         }
+//     }
+
+//     for (ll i = 2; i <= n; ++i)
+//     {
+//         if (is_prime[i])
+//         {
+//             primes[i] = true;
+//         }
+//     }
+// }
+
+// const int MAXN = 10000005;
+// ll a[MAXN] = {0};
+// int prime_factors(ll n, set<int> &s)
+// {
+//     int count = 0;
+//     if (n == 1)
+//         return 0;
+//     for (ll i = 2; i * i <= n; ++i)
+//     {
+//         if (n % i == 0)
+//         {
+//             count++;
+//             s.insert(i);
+//             while (n % i == 0)
+//             {
+//                 n /= i;
+//                 s.insert(i);
+//             }
+//         }
+//     }
+//     if (n > 1)
+//     {
+//         count++;
+//         s.insert(n);
+//     }
+//     return count;
+// }
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<int> a(n), b(m);
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> b[i];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (a[i] + b[j] <= k)
+                {
+                    ans++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+}
